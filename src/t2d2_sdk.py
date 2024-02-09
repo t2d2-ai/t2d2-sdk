9a940f753440f4076a2d5e8353f6e3fbefd3a165
Badri Hiriyur <badri@t2d2.ai>
2024-02-09 17:27:03 -0500

    Upload images supporting orthos

--- src/t2d2_sdk.py
+++ src/t2d2_sdk.py
@@ -348,11 +348,15 @@ class T2D2(object):
 
         # Upload images to S3
         assets = []
+        image_root = 'images'
+        if image_type == 3:
+            image_root = 'orthomosaics'
+
         for file_path in image_paths:
             base, ext = os.path.splitext(os.path.basename(file_path))
             filename = f"{base}_{random_string(6)}{ext}"
             s3_path = (
-                self.s3_base_url + f"/projects/{self.project['id']}/images/{filename}"
+                self.s3_base_url + f"/projects/{self.project['id']}/{image_root}/{filename}"
             )
             result = upload_file(file_path, s3_path)
             if result.get("success", False):
